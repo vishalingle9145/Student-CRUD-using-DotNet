e3198f7211c5d3e8166aec93955fa6cd39c162ac
Vishal <you@example.com>
2024-01-19 13:00:17 +0530

    initial add

---
+++ StudentPortal/StudentPortal.web/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentPortal.web.Models.Entities;
+
+namespace StudentPortal.web.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)
+        {
+            
+        }
+        public DbSet<Student> Students { get; set; }    
+    }
+}
